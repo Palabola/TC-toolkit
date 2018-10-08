3aa4064c344f1cf79e98daf97c26699decd7f55d
Palabola <Palabola@users.noreply.github.com>
2018-10-08 11:20:24 +0200

    Map Visualizer
    Map Visualizer

--- database_query_handler.js
+++ database_query_handler.js
@@ -3,11 +3,18 @@ require("dotenv").config();
 
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
+const pool_dev_db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB
+  database: process.env.MYSQL_DB_dev
+});
+
+const pool_world_db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB_world
 });
 
 const db_build = process.env.DB_BUILD;
@@ -15,7 +22,7 @@ const db_build = process.env.DB_BUILD;
 class query_handler {
   async select_item_template() {
     try {
-      let [rows, fields] = await pool.execute(
+      let [rows, fields] = await pool_dev_db.execute(
         "SELECT * FROM `db_itemsparse_" +
           db_build +
           "` as A inner join db_item_" +
@@ -30,8 +37,21 @@ class query_handler {
 
   async select_item_disenchantloot_template() {
     try {
-      let [rows, fields] = await pool.execute(
+      let [rows, fields] = await pool_dev_db.execute(
         "SELECT * FROM `db_itemdisenchantloot_" + db_build + "`"
+      );
+
+      return rows;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async select_creature_by_map(map) {
+    try {
+      let [rows, fields] = await pool_world_db.query(
+        "SELECT * FROM `creature` WHERE map = ?",
+        map
       );
 
       return rows;
@@ -42,7 +62,7 @@ class query_handler {
 
   async select_disenchant_loot_template() {
     try {
-      let [rows, fields] = await pool.execute(
+      let [rows, fields] = await pool_dev_db.execute(
         "SELECT * FROM `disenchant_loot_template`"
       );
 
@@ -54,7 +74,7 @@ class query_handler {
 
   async replace_disenchant_loot_template(object) {
     try {
-      let any = await pool.query(
+      let any = await pool_dev_db.query(
         "REPLACE INTO `disenchant_loot_template` SET ?;",
         object
       );
---
+++ map_spawn_visualizer.js
@@ -0,0 +1,61 @@
+const query_handler = require("./database_query_handler");
+const Jimp = require("Jimp");
+
+async function generate(map_id) {
+  let npc_spawns = await query_handler.select_creature_by_map(map_id);
+
+  let x_max = (x_min = npc_spawns[0].position_x);
+
+  let y_max = (y_min = npc_spawns[0].position_y);
+
+  npc_spawns.map(npc => {
+    if (npc.position_x > x_max) {
+      x_max = npc.position_x;
+    }
+    if (npc.position_x < x_min) {
+      x_min = npc.position_x;
+    }
+    if (npc.position_y > y_max) {
+      y_max = npc.position_y;
+    }
+    if (npc.position_y < y_min) {
+      y_min = npc.position_y;
+    }
+  });
+
+  console.log(x_max, y_max, x_min, y_min);
+  y_max;
+
+  let canvas_x = Math.abs(Math.floor(x_max - x_min));
+  let canvas_y = Math.abs(Math.floor(y_max - y_min));
+
+  console.log(canvas_x, canvas_y);
+
+  let white = 0xffffffff; // 0x + RGB + 256 Alpha
+
+  let image = new Jimp(canvas_x, canvas_y, "#000000", function(err, image) {
+    if (err) throw err;
+
+    npc_spawns.map(npc => {
+      let x = Math.floor(Math.abs(x_min) + npc.position_x);
+      let y = Math.floor(Math.abs(y_min) + npc.position_y);
+
+      image.setPixelColor(white, x - 1, y);
+      image.setPixelColor(white, x + 1, y);
+      image.setPixelColor(white, x, y + 1);
+      image.setPixelColor(white, x, y - 1);
+      image.setPixelColor(white, x - 2, y);
+      image.setPixelColor(white, x + 2, y);
+      image.setPixelColor(white, x, y + 2);
+      image.setPixelColor(white, x, y - 2);
+      image.setPixelColor(white, x, y);
+    });
+
+    image.write(map_id + "_map.png", err => {
+      if (err) throw err;
+      console.log("Generate done!");
+    });
+  });
+}
+
+generate(1642);
